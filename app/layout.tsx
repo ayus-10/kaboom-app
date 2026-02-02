c15fe8d870d76ab459f39ea52c3a5af16a4dd397
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2026-02-02 17:23:26 +0545

    query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { QueryProvider } from '@/providers/query-provider'
 import type { Metadata } from 'next'
 import './globals.css'
 
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   )
 }
